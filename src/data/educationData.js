18334630c14339ef8f37618a84803f81482a2168
khizarshah01 <khizar.shah010@gmail.com>
2024-09-17 22:08:55 +0530

    change profie pic, collage added ande theme

--- src/data/educationData.js
+++ src/data/educationData.js
@@ -3,7 +3,7 @@ export const educationData = [
         id: 1,
         institution: "St. Ann's English Medium High School, Khamgaon",
         course: 'Secondary School Certificate (SSC)',
-        startYear: '????',
+        startYear: '201*',
         endYear: '2021'
     },
     {
@@ -13,5 +13,11 @@ export const educationData = [
         startYear: '2021',
         endYear: '2024'
     },
-   
+    {
+        id: 3,
+        institution: 'Shri Sant Gajanan Maharaj College of Engineering (SSGMCE)',
+        course: 'B.E Computer Science course',
+        startYear: '2024',
+        endYear: '2027'
+    },
 ]
