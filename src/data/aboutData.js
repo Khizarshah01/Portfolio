62cabef3776b8b67d479c3b215b2a7a709ad3554
khizarshah01 <khizar.shah010@gmail.com>
2024-09-17 22:16:46 +0530

    change in some raw texts

--- src/data/aboutData.js
+++ src/data/aboutData.js
@@ -1,6 +1,6 @@
 export const aboutData = {
     title: "Who I am",
-    description1: "My name's khizar. I was born in Akola and grew up in Khamgaon. I did my schooling at St. Ann's English Medium High School and am now pursuing my Diploma from Siddhivinayak Technical Campus.",
+    description1: "My name's khizar. I was born in Akola and grew up in Khamgaon. I did my schooling at St. Ann's English Medium High School and I did my Diploma from Siddhivinayak Technical Campus and now I pursuing my B.E Degree in Shri Sant Gajanan Maharaj College of Engineering (SSGMCE) .",
     description2: "As a computer science student, I am driven by a passion for technology and a desire to make a positive impact through my work. With a strong foundation in programming, algorithms, and data structures, I am always looking to expand my skills and tackle new challenges. I am passionate about using technology to solve real-world problems and create innovative solutions. In my free time, I enjoy experimenting with new technologies and contributing to open-source projects.",
      image: 2
 }
--- src/data/educationData.js
+++ src/data/educationData.js
@@ -1,6 +1,13 @@
 export const educationData = [
     {
         id: 1,
+        institution: 'Shri Sant Gajanan Maharaj College of Engineering (SSGMCE)',
+        course: 'B.E Computer Science course',
+        startYear: '2024',
+        endYear: '2027'
+    },
+    {
+        id: 3,
         institution: "St. Ann's English Medium High School, Khamgaon",
         course: 'Secondary School Certificate (SSC)',
         startYear: '201*',
@@ -12,12 +19,5 @@ export const educationData = [
         course: 'Diploma Computer Science course',
         startYear: '2021',
         endYear: '2024'
-    },
-    {
-        id: 3,
-        institution: 'Shri Sant Gajanan Maharaj College of Engineering (SSGMCE)',
-        course: 'B.E Computer Science course',
-        startYear: '2024',
-        endYear: '2027'
     },
 ]
